b3efdbd6af252709eb2d98631f1a3ec3ccadcb2c
hasib824 <hasib824@gmail.com>
2016-04-30 02:50:49 +0600

    Added files via upload

---
+++ Automaticexpression.java
@@ -0,0 +1,15 @@
+
+package automaticexpression;
+
+public class Automaticexpression {
+
+    public static void main(String[] args) {
+        
+        byte val1=3,val2=2,val3=8;
+        byte res=24;
+        
+        System.out.println("The result is "+ res);
+       
+    }
+    
+}
